8616cd765d67f93fb17f19f15e7ea81c466e2344
cosmoshane <37876482+cosmoshane@users.noreply.github.com>
2021-03-14 14:19:19 +0530

    firebase user registeration

--- app/src/main/java/com/example/doodle_war/login/user_registration.java
+++ app/src/main/java/com/example/doodle_war/login/user_registration.java
@@ -1,26 +1,87 @@
 package com.example.doodle_war.login;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.doodle_war.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class user_registration extends AppCompatActivity {
 
-    EditText email,password,confirmpassword;
-    Button createuser;
+    EditText UserEmail, UserPassword, UserConfirmpassword;
+    Button CreateUser;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog loadingBar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_registration);
-        email=findViewById(R.id.email);
-        password=findViewById(R.id.password);
-        confirmpassword=findViewById(R.id.confirmpassword);
-        createuser=findViewById(R.id.createuser);
+        firebaseAuth = FirebaseAuth.getInstance();
+        UserEmail = findViewById(R.id.email);
+        UserPassword = findViewById(R.id.password);
+        UserConfirmpassword = findViewById(R.id.confirmpassword);
+        CreateUser = findViewById(R.id.createuser);
+        loadingBar =  new ProgressDialog(this);
+
+        CreateUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CreateNewAccount();
+            }
+
+
+        });
+    }
+
+    private void CreateNewAccount() {
+        String email = UserEmail.getText().toString();
+        String password = UserPassword.getText().toString();
+        String confirmPassword = UserConfirmpassword.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Write Your Email", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Write Your Email", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(confirmPassword)) {
+            Toast.makeText(this, "Please confirm your Password", Toast.LENGTH_SHORT).show();
+        } else if (!password.equals(confirmPassword)) {
+            Toast.makeText(this, "Both password doesn't match", Toast.LENGTH_SHORT).show();
+        } else {
+            loadingBar.setTitle("Creating New Account");
+            loadingBar.setMessage("Please wait account is creating");
+            loadingBar.show();
+            loadingBar.setCanceledOnTouchOutside(true);
+            firebaseAuth.createUserWithEmailAndPassword(email,password)
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful())
+                            {
+                                Toast.makeText(user_registration.this,"Authentication Successful",Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                            else
+                            {
+                                String message = task.getException().getMessage();
+                                Toast.makeText(user_registration.this,"Error Occured" + message,Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                            }
+                        }
+                    });
+
+        }
     }
 }
